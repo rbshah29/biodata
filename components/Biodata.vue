1cda82d15e7cf17ec39cf6250b635b57a1afb22d
Rutvik Shah <55599346+rbshah29@users.noreply.github.com>
2024-06-27 07:28:58 -0400

    Update Biodata.vue
    Changes

--- components/Biodata.vue
+++ components/Biodata.vue
@@ -31,8 +31,12 @@
                   <h2 class="text-2xl font-semibold text-gray-700">Contact Information</h2><hr class="border-gray-400" />
                   <ul class="space-y-4 mt-2 mb-2">
                     <li class="flex items-center">
-                      <svg class="w-5 h-5 text-gray-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
-                      <span>rbshah0029@gmail.com</span>
+                       <a href="mailto:example@example.com" style="text-decoration: none; color: #000000;">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+        <polyline points="22,6 12,13 2,6"></polyline>
+    </svg> rbshah0029@gmail.com
+</a>
                     </li>
                     <li class="flex items-center">
                       <svg class="w-5 h-5 text-gray-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
