14fa5799940d9ce892b852ddd284dfa17e08bced
Rutvik Shah <55599346+rbshah29@users.noreply.github.com>
2024-06-27 07:31:00 -0400

    Update Biodata.vue
    Exampla

--- components/Biodata.vue
+++ components/Biodata.vue
@@ -31,7 +31,7 @@
                   <h2 class="text-2xl font-semibold text-gray-700">Contact Information</h2><hr class="border-gray-400" />
                   <ul class="space-y-4 mt-2 mb-2">
                     <li class="flex items-center">
-                       <a href="mailto:example@example.com" style="text-decoration: none; color: #000000;">
+                       <a href="mailto:rbshah0029@gmail.com" style="text-decoration: none; color: #000000;">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
         <polyline points="22,6 12,13 2,6"></polyline>
